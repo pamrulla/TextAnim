c1ce566e122e33c679a565ea177e9ee8595f261d
Patan Amrulla Khan <pamrulla@gmail.com>
2017-06-03 14:24:47 +0530

    Updated to delta values Added empty as parent to all text objects

--- TextAnim.py
+++ TextAnim.py
@@ -28,76 +28,90 @@ class TextAnimOne(bpy.types.Operator):
         bpy.ops.object.text_add()
         text1 = bpy.context.active_object
         text1.data.align_x = 'CENTER'
-        text1.location.y = text1.location.y  + 0.2
+        text1.delta_location.y = text1.delta_location.y  + 0.2
         text1.data.body ="Khan"
         text1.name = "khan"
 
         bpy.ops.object.text_add()
         text2 = bpy.context.active_object
         text2.data.align_x = 'CENTER'
-        text2.location.y = text1.location.y  - 1.2
+        text2.delta_location.y = text1.delta_location.y  - 1.2
         text2.data.body ="Amrulla"
         text2.name = "amrulla"
+        
+        bpy.ops.object.empty_add()
+        empty = bpy.context.active_object
+        empty.name = "textMain"
+        empty.empty_draw_type = 'CIRCLE'
+        empty.rotation_euler.x = radians(90)
+        
+        bpy.ops.object.select_all(action='DESELECT')
+        text1.select = True
+        text2.select = True
+        empty.select = True        
+        bpy.ops.object.parent_set(type='OBJECT', keep_transform=True)
+        bpy.ops.object.select_all(action='DESELECT')
 
-        text1.keyframe_insert(data_path="location", frame=0.0)
-        text1.keyframe_insert(data_path="location", frame=24.0)
-        text1.keyframe_insert(data_path="location", frame=72.0)
-        text1.keyframe_insert(data_path="rotation_euler", frame=0.0)
-        text1.keyframe_insert(data_path="rotation_euler", frame=24.0)
-        text1.keyframe_insert(data_path="rotation_euler", frame=72.0)
-        text1.keyframe_insert(data_path="scale", frame=0.0)
-        text1.keyframe_insert(data_path="scale", frame=24.0)
-        text1.keyframe_insert(data_path="scale", frame=72.0)
+        text1.keyframe_insert(data_path="delta_location", frame=0.0)
+        text1.keyframe_insert(data_path="delta_location", frame=24.0)
+        text1.keyframe_insert(data_path="delta_location", frame=72.0)
+        text1.keyframe_insert(data_path="delta_rotation_euler", frame=0.0)
+        text1.keyframe_insert(data_path="delta_rotation_euler", frame=24.0)
+        text1.keyframe_insert(data_path="delta_rotation_euler", frame=72.0)
+        text1.keyframe_insert(data_path="delta_scale", frame=0.0)
+        text1.keyframe_insert(data_path="delta_scale", frame=24.0)
+        text1.keyframe_insert(data_path="delta_scale", frame=72.0)
 
-        text1.location.x += 2.0
-        text1.location.y += 0.25
-        text1.keyframe_insert(data_path="location", frame=1.0)
-        text1.keyframe_insert(data_path="location", frame=96.0)
-        text1.scale = (0.001, 0.001, 0.001)
-        text1.keyframe_insert(data_path="scale", frame=1.0)
-        text1.keyframe_insert(data_path="scale", frame=96.0)
-        text1.rotation_euler.z = radians(45)
-        text1.rotation_euler.y = radians(90)
-        text1.keyframe_insert(data_path="rotation_euler", frame=1.0)
-        text1.keyframe_insert(data_path="rotation_euler", frame=96.0)
+        text1.delta_location.x += 2.0
+        text1.delta_location.y += 0.25
+        text1.keyframe_insert(data_path="delta_location", frame=1.0)
+        text1.keyframe_insert(data_path="delta_location", frame=96.0)
+        text1.delta_scale = (0.001, 0.001, 0.001)
+        text1.keyframe_insert(data_path="delta_scale", frame=1.0)
+        text1.keyframe_insert(data_path="delta_scale", frame=96.0)
+        text1.delta_rotation_euler.z = radians(45)
+        text1.delta_rotation_euler.y = radians(90)
+        text1.keyframe_insert(data_path="delta_rotation_euler", frame=1.0)
+        text1.keyframe_insert(data_path="delta_rotation_euler", frame=96.0)
 
-        text2.keyframe_insert(data_path="location", frame=24.0)
-        text2.keyframe_insert(data_path="location", frame=0.0)
-        text2.keyframe_insert(data_path="location", frame=72.0)
-        text2.keyframe_insert(data_path="rotation_euler", frame=24.0)
-        text2.keyframe_insert(data_path="rotation_euler", frame=0.0)
-        text2.keyframe_insert(data_path="rotation_euler", frame=72.0)
-        text2.keyframe_insert(data_path="scale", frame=24.0)
-        text2.keyframe_insert(data_path="scale", frame=0.0)
-        text2.keyframe_insert(data_path="scale", frame=72.0)
+        text2.keyframe_insert(data_path="delta_location", frame=24.0)
+        text2.keyframe_insert(data_path="delta_location", frame=0.0)
+        text2.keyframe_insert(data_path="delta_location", frame=72.0)
+        text2.keyframe_insert(data_path="delta_rotation_euler", frame=24.0)
+        text2.keyframe_insert(data_path="delta_rotation_euler", frame=0.0)
+        text2.keyframe_insert(data_path="delta_rotation_euler", frame=72.0)
+        text2.keyframe_insert(data_path="delta_scale", frame=24.0)
+        text2.keyframe_insert(data_path="delta_scale", frame=0.0)
+        text2.keyframe_insert(data_path="delta_scale", frame=72.0)
 
-        text2.location.x -= 2.0
-        text2.location.y -= 0.25
-        text2.keyframe_insert(data_path="location", frame=1.0)
-        text2.keyframe_insert(data_path="location", frame=96.0)
-        text2.scale = (0.001, 0.001, 0.001)
-        text2.keyframe_insert(data_path="scale", frame=1.0)
-        text2.keyframe_insert(data_path="scale", frame=96.0)
-        text2.rotation_euler.z = radians(-45)
-        text2.rotation_euler.y = radians(-90)
-        text2.keyframe_insert(data_path="rotation_euler", frame=1.0)
-        text2.keyframe_insert(data_path="rotation_euler", frame=96.0)
+        text2.delta_location.x -= 2.0
+        text2.delta_location.y -= 0.25
+        text2.keyframe_insert(data_path="delta_location", frame=1.0)
+        text2.keyframe_insert(data_path="delta_location", frame=96.0)
+        text2.delta_scale = (0.001, 0.001, 0.001)
+        text2.keyframe_insert(data_path="delta_scale", frame=1.0)
+        text2.keyframe_insert(data_path="delta_scale", frame=96.0)
+        text2.delta_rotation_euler.z = radians(-45)
+        text2.delta_rotation_euler.y = radians(-90)
+        text2.keyframe_insert(data_path="delta_rotation_euler", frame=1.0)
+        text2.keyframe_insert(data_path="delta_rotation_euler", frame=96.0)
 
         bpy.ops.object.select_all(action='DESELECT')
         bpy.context.scene.frame_set(1)
 
-        """text1.keyframe_delete(data_path="location", frame=1.0)
-        text1.keyframe_delete(data_path="location", frame=24.0)
-        text1.keyframe_delete(data_path="location", frame=72.0)
-        text1.keyframe_delete(data_path="location", frame=96.0)
-        text1.keyframe_delete(data_path="rotation_euler", frame=1.0)
-        text1.keyframe_delete(data_path="rotation_euler", frame=24.0)
-        text1.keyframe_delete(data_path="rotation_euler", frame=72.0)
-        text1.keyframe_delete(data_path="rotation_euler", frame=96.0)
-        text1.keyframe_delete(data_path="scale", frame=1.0)
-        text1.keyframe_delete(data_path="scale", frame=24.0)
-        text1.keyframe_delete(data_path="scale", frame=72.0)
-        text1.keyframe_delete(data_path="scale", frame=96.0)
+        """
+        text1.keyframe_delete(data_path="delta_location", frame=1.0)
+        text1.keyframe_delete(data_path="delta_location", frame=24.0)
+        text1.keyframe_delete(data_path="delta_location", frame=72.0)
+        text1.keyframe_delete(data_path="delta_location", frame=96.0)
+        text1.keyframe_delete(data_path="delta_rotation_euler", frame=1.0)
+        text1.keyframe_delete(data_path="delta_rotation_euler", frame=24.0)
+        text1.keyframe_delete(data_path="delta_rotation_euler", frame=72.0)
+        text1.keyframe_delete(data_path="delta_rotation_euler", frame=96.0)
+        text1.keyframe_delete(data_path="delta_scale", frame=1.0)
+        text1.keyframe_delete(data_path="delta_scale", frame=24.0)
+        text1.keyframe_delete(data_path="delta_scale", frame=72.0)
+        text1.keyframe_delete(data_path="delta_scale", frame=96.0)
         """
         
         return {'FINISHED'}
@@ -119,7 +133,7 @@ def register():
     bpy.utils.register_module(__name__)
 
 def unregister():
-    bpy.utils.register_module(__name__)
+    bpy.utils.unregister_module(__name__)
     
 def menu_func(self, context):
     self.layout.operator(TextAnim.bl_idname, icon='MESH_CUBE')
