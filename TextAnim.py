012f3ea08e2bc3bc03a4d9fef3f7f307bf208ab6
Patan Amrulla Khan <pamrulla@gmail.com>
2017-06-07 19:36:31 +0530

    Reframed complete code to be modular

--- TextAnim.py
+++ TextAnim.py
@@ -1,5 +1,5 @@
 import bpy
-from bpy.props import BoolProperty, StringProperty
+from bpy.props import BoolProperty, StringProperty, EnumProperty
 from math import radians
 
 bl_info = {
@@ -15,122 +15,113 @@ bl_info = {
     "category": "Animation"
 }
 
+styles_enum = [("0", "Anim One", "This is style one"),
+    ("1", "Two One", "This is style one")]
+
+bpy.types.Scene.selectionTextAnimStyle = EnumProperty(
+        items=styles_enum,
+        name="Animation Types")
+
+keys_full = [
+        [
+            [
+                {'loc':(0, 0.2, 0), 'rot':(0, 0, 0), 'sca':(1, 1, 1), 'frame':0},
+                {'loc':(2, 0.45, 0), 'rot':(0, radians(90), radians(45)), 'sca':(0.001, 0.001, 0.001), 'frame':1},
+                {'loc':(0, 0.2, 0), 'rot':(0, 0, 0), 'sca':(1, 1, 1), 'frame':24},
+                {'loc':(0, 0.2, 0), 'rot':(0, 0, 0), 'sca':(1, 1, 1), 'frame':72},
+                {'loc':(2, 0.45, 0), 'rot':(0, radians(90), radians(45)), 'sca':(0.001, 0.001, 0.001), 'frame':96}
+             ],
+             [
+                {'loc':(0, -1.2, 0), 'rot':(0, 0, 0), 'sca':(1, 1, 1), 'frame':0},
+                {'loc':(-2, -1.45, 0), 'rot':(0, radians(-90), radians(-45)), 'sca':(0.001, 0.001, 0.001), 'frame':1},
+                {'loc':(0, -1.2, 0), 'rot':(0, 0, 0), 'sca':(1, 1, 1), 'frame':24},
+                {'loc':(0, -1.2, 0), 'rot':(0, 0, 0), 'sca':(1, 1, 1), 'frame':72},
+                {'loc':(-2, -1.45, 0), 'rot':(0, radians(-90), radians(-45)), 'sca':(0.001, 0.001, 0.001), 'frame':96}
+             ]
+        ]
+     ]
+
+"""with open("test", 'w') as f:
+    json.dump(keys_full, f)
+keys_full = None
+ 
+with open("test", 'r') as f:
+    global keys_full
+    keys_full = json.load(f)"""
+
+def addKeyFrames(obj, dataList):
+    for data in dataList:
+        obj.delta_location = data['loc']
+        obj.delta_rotation_euler = data['rot']
+        obj.delta_scale = data['sca']
+        obj.keyframe_insert(data_path="delta_location", frame=data['frame'])
+        obj.keyframe_insert(data_path="delta_rotation_euler", frame=data['frame'])
+        obj.keyframe_insert(data_path="delta_scale", frame=data['frame'])
+
+def addTextObject(text):
+    bpy.ops.object.text_add()
+    text1 = bpy.context.active_object
+    text1.data.align_x = 'CENTER'
+    text1.data.body = text
+    text1.name = text
+    return text1
+    
+def addEmptyObject(text):
+    bpy.ops.object.empty_add()
+    empty = bpy.context.active_object
+    empty.name = text
+    empty.empty_draw_type = 'CIRCLE'
+    empty.rotation_euler.x = radians(90)
+    #empty.textAnim.textAnim = True
+    return empty
+        
+def makeParent(empty, texts):
+    bpy.ops.object.select_all(action='DESELECT')
+    for text in texts:
+        text.select = True
+    empty.select = True
+    bpy.ops.object.parent_set(type='OBJECT', keep_transform=True)
+    bpy.ops.object.select_all(action='DESELECT')
+
+def setCurrentFrame():
+    bpy.ops.object.select_all(action='DESELECT')
+    bpy.context.scene.frame_set(1)
+
+class TextAnimCreate(bpy.types.Operator):
+    """Creates pre-made text animations"""
+    bl_idname = "text_anim.create"
+    bl_label = "Create"
+    bl_options = {'REGISTER', 'UNDO'}
+    bl_context = "objectmode"
+
+    def execute(self, context):
+        if context.scene.selectionTextAnimStyle == "0":
+            bpy.ops.object.select_all(action='SELECT')
+            bpy.ops.object.delete(use_global=False)
+
+            text_objs = []
+            
+            i = 1
+            for data in keys_full[int(context.scene.selectionTextAnimStyle)]:
+                text_objs.append(addTextObject('text'+str(i)))
+                addKeyFrames(text_objs[i-1], data)
+                i += 1
+            
+            empty = addEmptyObject("AnimMain " + context.scene.selectionTextAnimStyle)
+            
+            makeParent(empty, text_objs)
+            
+            setCurrentFrame()
+        
+        return {'FINISHED'}
+
+"""
 class TextAnimPropertyGroup(bpy.types.PropertyGroup):
     textAnim = BoolProperty(name="TextAnim", default=False)
     text1 = StringProperty(name="Text1", default="")
     text2 = StringProperty(name="Text1", default="")
 
-class TextAnimOne(bpy.types.Operator):
-    """Creates pre-made text animations"""
-    bl_idname = "text_anim.pone"
-    bl_label = "Text 001"
-    bl_options = {'REGISTER', 'UNDO'}
-    bl_context = "objectmode"
-
-    def execute(self, context):
-        #bpy.ops.object.select_all(action='SELECT')
-        #bpy.ops.object.delete(use_global=False)
-
-        bpy.ops.object.text_add()
-        text1 = bpy.context.active_object
-        text1.data.align_x = 'CENTER'
-        text1.delta_location.y = text1.delta_location.y  + 0.2
-        text1.data.body ="Khan"
-        
-
-        bpy.ops.object.text_add()
-        text2 = bpy.context.active_object
-        text2.data.align_x = 'CENTER'
-        text2.delta_location.y = text1.delta_location.y  - 1.2
-        text2.data.body ="Amrulla"
-        
-        
-        bpy.ops.object.empty_add()
-        empty = bpy.context.active_object
-        empty.name = "textMain"
-        empty.empty_draw_type = 'CIRCLE'
-        empty.rotation_euler.x = radians(90)
-        empty.textAnim.textAnim = True
-        
-        text1.name = empty.name + "_text1"
-        text2.name = empty.name + "_text2"
-        
-        empty.textAnim.text1 = text1.name
-        empty.textAnim.text2 = text2.name        
-        
-        bpy.ops.object.select_all(action='DESELECT')
-        text1.select = True
-        text2.select = True
-        empty.select = True        
-        bpy.ops.object.parent_set(type='OBJECT', keep_transform=True)
-        bpy.ops.object.select_all(action='DESELECT')
-
-        text1.keyframe_insert(data_path="delta_location", frame=0.0)
-        text1.keyframe_insert(data_path="delta_location", frame=24.0)
-        text1.keyframe_insert(data_path="delta_location", frame=72.0)
-        text1.keyframe_insert(data_path="delta_rotation_euler", frame=0.0)
-        text1.keyframe_insert(data_path="delta_rotation_euler", frame=24.0)
-        text1.keyframe_insert(data_path="delta_rotation_euler", frame=72.0)
-        text1.keyframe_insert(data_path="delta_scale", frame=0.0)
-        text1.keyframe_insert(data_path="delta_scale", frame=24.0)
-        text1.keyframe_insert(data_path="delta_scale", frame=72.0)
-
-        text1.delta_location.x += 2.0
-        text1.delta_location.y += 0.25
-        text1.keyframe_insert(data_path="delta_location", frame=1.0)
-        text1.keyframe_insert(data_path="delta_location", frame=96.0)
-        text1.delta_scale = (0.001, 0.001, 0.001)
-        text1.keyframe_insert(data_path="delta_scale", frame=1.0)
-        text1.keyframe_insert(data_path="delta_scale", frame=96.0)
-        text1.delta_rotation_euler.z = radians(45)
-        text1.delta_rotation_euler.y = radians(90)
-        text1.keyframe_insert(data_path="delta_rotation_euler", frame=1.0)
-        text1.keyframe_insert(data_path="delta_rotation_euler", frame=96.0)
-
-        text2.keyframe_insert(data_path="delta_location", frame=24.0)
-        text2.keyframe_insert(data_path="delta_location", frame=0.0)
-        text2.keyframe_insert(data_path="delta_location", frame=72.0)
-        text2.keyframe_insert(data_path="delta_rotation_euler", frame=24.0)
-        text2.keyframe_insert(data_path="delta_rotation_euler", frame=0.0)
-        text2.keyframe_insert(data_path="delta_rotation_euler", frame=72.0)
-        text2.keyframe_insert(data_path="delta_scale", frame=24.0)
-        text2.keyframe_insert(data_path="delta_scale", frame=0.0)
-        text2.keyframe_insert(data_path="delta_scale", frame=72.0)
-
-        text2.delta_location.x -= 2.0
-        text2.delta_location.y -= 0.25
-        text2.keyframe_insert(data_path="delta_location", frame=1.0)
-        text2.keyframe_insert(data_path="delta_location", frame=96.0)
-        text2.delta_scale = (0.001, 0.001, 0.001)
-        text2.keyframe_insert(data_path="delta_scale", frame=1.0)
-        text2.keyframe_insert(data_path="delta_scale", frame=96.0)
-        text2.delta_rotation_euler.z = radians(-45)
-        text2.delta_rotation_euler.y = radians(-90)
-        text2.keyframe_insert(data_path="delta_rotation_euler", frame=1.0)
-        text2.keyframe_insert(data_path="delta_rotation_euler", frame=96.0)
-
-        bpy.ops.object.select_all(action='DESELECT')
-        bpy.context.scene.frame_set(1)
-
-        """
-        text1.keyframe_delete(data_path="delta_location", frame=1.0)
-        text1.keyframe_delete(data_path="delta_location", frame=24.0)
-        text1.keyframe_delete(data_path="delta_location", frame=72.0)
-        text1.keyframe_delete(data_path="delta_location", frame=96.0)
-        text1.keyframe_delete(data_path="delta_rotation_euler", frame=1.0)
-        text1.keyframe_delete(data_path="delta_rotation_euler", frame=24.0)
-        text1.keyframe_delete(data_path="delta_rotation_euler", frame=72.0)
-        text1.keyframe_delete(data_path="delta_rotation_euler", frame=96.0)
-        text1.keyframe_delete(data_path="delta_scale", frame=1.0)
-        text1.keyframe_delete(data_path="delta_scale", frame=24.0)
-        text1.keyframe_delete(data_path="delta_scale", frame=72.0)
-        text1.keyframe_delete(data_path="delta_scale", frame=96.0)
-        """
-        
-        return {'FINISHED'}
-
 class TextAnimOneUpdate(bpy.types.Operator):
-    """Updates the animation"""
     bl_idname = "pone.update"
     bl_label = "Update"
     bl_options = {'REGISTER'}
@@ -170,6 +161,7 @@ class TextAnimPOne(bpy.types.Panel):
             box.prop(anim, "text1")
             box.prop(anim, "text2")
             box.operator("pone.update")
+"""
     
 class TextAnim(bpy.types.Panel):
     bl_idname = "OBJECT_PT_text_anim"
@@ -182,11 +174,12 @@ class TextAnim(bpy.types.Panel):
     def draw(self, context):
         box = self.layout.box()
         box.label(text = "Tools")
-        box.operator("text_anim.pone")
+        box.prop(context.scene, 'selectionTextAnimStyle')
+        box.operator("text_anim.create")
         
 def register():
     bpy.utils.register_module(__name__)
-    bpy.types.Object.textAnim = bpy.props.PointerProperty(type=TextAnimPropertyGroup)
+    #bpy.types.Object.textAnim = bpy.props.PointerProperty(type=TextAnimPropertyGroup)
 
 def unregister():
     bpy.utils.unregister_module(__name__)
