10f65a68695ea67a4390561faedc70ab788f5778
Patan Amrulla Khan <pamrulla@gmail.com>
2017-06-07 22:59:50 +0530

    Added first full animation

--- TextAnim.py
+++ TextAnim.py
@@ -22,7 +22,7 @@ bpy.types.Scene.selectionTextAnimStyle = EnumProperty(
         items=styles_enum,
         name="Animation Types")
 
-keys_full = [
+"""keys_full = [
         [
             [
                 {'loc':(0, 0.2, 0), 'rot':(0, 0, 0), 'sca':(1, 1, 1), 'frame':0},
@@ -39,7 +39,64 @@ keys_full = [
                 {'loc':(-2, -1.45, 0), 'rot':(0, radians(-90), radians(-45)), 'sca':(0.001, 0.001, 0.001), 'frame':96}
              ]
         ]
-     ]
+     ]"""
+keys_full = [
+    [
+        [
+            {'loc':( 0, 0.85, 0), 'rot':( 0, 0, 0), 'sca':( 1, 1, 1), 'frame':0},
+            {'loc':( 0, 0.6, 0), 'rot':( -180, 0, 0), 'sca':( 1, 0.001, 1), 'frame':1},
+            {'rot':( -180, 0, 0), 'sca':( 1, 1, 1), 'frame':5},
+            {'loc':( 0, 0.6, 0), 'rot':( 0, 0, 0), 'frame':15},
+            {'loc':( 0, 0.43, 0), 'frame':20},
+            {'loc':( 0, 0.43, 0), 'frame':25},
+            {'loc':( 0, 0.85, 0), 'rot':( 0, 0, 0), 'sca':( 1, 1, 1), 'frame':31},
+            {'loc':( 0, 1, 0), 'frame':40},
+            {'loc':( 0, 1, 0), 'rot':( 0, 0, 0), 'sca':( 1, 1, 1), 'frame':43},
+            {'loc':( 0, 0.85, 0), 'frame':55},
+            {'loc':( 0, 0.85, 0), 'frame':85},
+            {'loc':( 0, 1, 0), 'frame':97},
+            {'loc':( 0, 1, 0), 'frame':101},
+            {'loc':( 0, 0.43, 0), 'frame':121},
+            {'loc':( 0, 0.6, 0), 'rot':( 0, 0, 0), 'frame':127},
+            {'loc':( 0, 0.85, 0), 'rot':( -180, 0, 0), 'sca':( 1, 1, 1), 'frame':132},
+            {'sca':( 1, 0.001, 1), 'frame':136}
+        ],
+        [
+            {'loc':( 0, 0, 0), 'rot':( 0, 0, 0), 'sca':( 1, 1, 1), 'frame':0},
+            {'loc':( 0, 0, 0), 'rot':( -180, 0, 0), 'sca':( 1, 0.001, 1), 'frame':1},
+            {'sca':( 1, 0.001, 1), 'frame':20},
+            {'rot':( -180, 0, 0), 'sca':( 1, 0.001, 1), 'frame':25},
+            {'rot':( -180, 0, 0), 'sca':( 1, 1, 1), 'frame':31},
+            {'rot':( 0, 0, 0), 'sca':( 1.5, 1.5, 1), 'frame':40},
+            {'loc':( 0, 0, 0), 'rot':( 0, 0, 0), 'sca':( 1.5, 1.5, 1), 'frame':43},
+            {'sca':( 1, 1, 1), 'frame':55},
+            {'sca':( 1, 1, 1), 'frame':85},
+            {'sca':( 1.5, 1.5, 1), 'frame':97},
+            {'rot':( 0, 0, 0), 'sca':( 1.5, 1.5, 1), 'frame':101},
+            {'rot':( -180, 0, 0), 'sca':( 1, 1, 1), 'frame':110},
+            {'sca':( 1, 0.001, 1), 'frame':116},
+        ],
+        [
+            {'loc':( 0, -0.85, 0), 'rot':( 0, 0, 0), 'sca':( 1, 1, 1), 'frame':0},
+            {'loc':( 0, -0.6, 0), 'rot':( -180, 0, 0), 'sca':( 1, 0.001, 1), 'frame':1},
+            {'rot':( -180, 0, 0), 'sca':( 1, 1, 1), 'frame':5},
+            {'loc':( 0, -0.6, 0), 'rot':( 0, 0, 0), 'frame':15},
+            {'loc':( 0, -0.43, 0), 'frame':20},
+            {'loc':( 0, -0.43, 0), 'frame':25},
+            {'loc':( 0, -0.85, 0), 'rot':( 0, 0, 0), 'sca':( 1, 1, 1), 'frame':31},
+            {'loc':( 0, -1, 0), 'frame':40},
+            {'loc':( 0, -1, 0), 'rot':( 0, 0, 0), 'sca':( 1, 1, 1), 'frame':43},
+            {'loc':( 0, -0.85, 0), 'frame':55},
+            {'loc':( 0, -0.85, 0), 'frame':85},
+            {'loc':( 0, -1, 0), 'frame':97},
+            {'loc':( 0, -1, 0), 'frame':101},
+            {'loc':( 0, -0.43, 0), 'frame':121},
+            {'loc':( 0, -0.6, 0), 'rot':( 0, 0, 0), 'frame':127},
+            {'loc':( 0, -0.85, 0), 'rot':( -180, 0, 0), 'sca':( 1, 1, 1), 'frame':132},
+            {'sca':( 1, 0.001, 1), 'frame':136}
+        ]
+    ]
+]
 
 """with open("test", 'w') as f:
     json.dump(keys_full, f)
@@ -51,9 +108,14 @@ with open("test", 'r') as f:
 
 def addKeyFrames(obj, dataList):
     for data in dataList:
-        obj.delta_location = data['loc']
-        obj.delta_rotation_euler = data['rot']
-        obj.delta_scale = data['sca']
+        if 'loc' in data:
+            obj.delta_location = data['loc']
+        if 'rot' in data:
+            obj.delta_rotation_euler.x = radians(data['rot'][0])
+            obj.delta_rotation_euler.y = radians(data['rot'][1])
+            obj.delta_rotation_euler.z = radians(data['rot'][2])
+        if 'sca' in data:
+            obj.delta_scale = data['sca']
         obj.keyframe_insert(data_path="delta_location", frame=data['frame'])
         obj.keyframe_insert(data_path="delta_rotation_euler", frame=data['frame'])
         obj.keyframe_insert(data_path="delta_scale", frame=data['frame'])
@@ -64,6 +126,7 @@ def addTextObject(text):
     text1.data.align_x = 'CENTER'
     text1.data.body = text
     text1.name = text
+    bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
     return text1
     
 def addEmptyObject(text):
